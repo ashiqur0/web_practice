effc9ceed6134841a7bc08ce101858280209fad2
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-09-03 10:42:42 +0600

    API, JSON, Data Load, Dynamic Website

--- Milestone_6/Module_32/script.js
+++ Milestone_6/Module_32/script.js
@@ -1,11 +1,13 @@
+const post_url = 'https://jsonplaceholder.typicode.com/posts';
 document.getElementById('btn').addEventListener('click', function() {
-  const url = 'https://jsonplaceholder.typicode.com/posts';
-    fetch(url)
+    fetch(post_url)
       .then(response => response.json())
       .then(json => loadData(json));
+      // .then(json => console.log(json));
 });
 
 const loadData = (data) => {
+  document.getElementById('card-container').innerText = '';
   data.forEach(element => {
     const div = document.createElement('div');
     div.innerHTML = `
@@ -15,6 +17,31 @@ const loadData = (data) => {
     
     </li>
     
+    `
+    document.getElementById('card-container').appendChild(div);
+  });
+}
+
+const comment_url = 'https://jsonplaceholder.typicode.com/comments';
+document.getElementById('btn2').addEventListener('click', function() {
+    fetch(comment_url)
+      .then(response => response.json())
+      .then(json => {
+        loadData2(json)
+      });
+});
+
+const loadData2 = (data) => {
+  document.getElementById('card-container').innerText = '';
+  let i = 1;
+  data.forEach(element => {
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <li class="w-5/6 h-full p-2.5 bg-white mb-4 rounded-2xl mx-auto shadow-xl">
+      <h1 class="text-red-400 text-2xl font-semibold">${i++} ${element.name}</h1>
+      <p class='text-purple-600 mt-2'>${element.email}</p>
+      <p class='text-gray-600 mt-2'>${element.body}</p>    
+    </li>    
     `
     document.getElementById('card-container').appendChild(div);
   });
