5b1728cc19890a160a02a9134e1c733f09192e90
ashiqur0 <ashiqurrahmans2018@gmail.com>
2025-09-10 22:52:07 +0600

    Form Practice

--- Milestone_3/Module_17/assignment3.js
+++ Milestone_3/Module_17/assignment3.js
@@ -1,11 +1,9 @@
 /** Problem -01 ( Divide the Asset ) */
 var area = 800;
-//write your code here
 console.log(area / 2);
 
 /** Problem -02 ( Cycle or Laptop ) */
 var money = 10000;
-//write your code here
 if (money >= 25000) {
     console.log("Laptop");
 } else if (money >= 10000) {
@@ -16,14 +14,12 @@ if (money >= 25000) {
 
 /** Problem -03 ( Medicine Planner ) */
 var lastDay = 11 ;
-//write your code here
 for (var i = 1; i <= lastDay; i++) {
     i % 3 === 0? console.log(i + " - medicine") : console.log(i + " - rest");
 }
 
 /** Problem 04 - (Delete / Store) */
 var fileName= "pdfData.jpg";
-//write your code here
 if (fileName[0] == '#') {
     console.log("Store");
 } else {
@@ -37,13 +33,11 @@ if (fileName[0] == '#') {
 
 /** Problem 05 - ( PH Email Generator )  */
 var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
-//write your code here
 console.log(student.name + student.roll + "." + student.department + "@ph.ac.bd");
 
 /** Problem 06 :  (Current Salary )  */
 var experience = 30;
 var startingSalary = 45000;
-//write your code here
 while (experience--) {
     startingSalary += startingSalary * 5 / 100;
 }
